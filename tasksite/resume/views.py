b1a247f158ff0ef2d487703977fc249307c6f309
KnjazevFedor <85475638+KnjazevFedor@users.noreply.github.com>
2021-06-07 19:31:47 +0700

    Update views.py

--- tasksite/resume/views.py
+++ tasksite/resume/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+fill = {"Желаемая должность": "Плотник", "Опыт работы": "5 лет",
+        "Образование": "Среднее образование", "Курсы": "-",
+        "Ключевые навыки": "Изготовление мебели", "Дополнительная информация": "Добросовестный"}
+
+fill = fill.items()
+
 
 # Create your views here.
 def index(request):  # HttpRequest
-    return render(request, "resume/Base.html", {"name": "Иван Васильевич", "birthday": "12.02.84", "job": "Плотник",
-                                                "exp": "5 лет", "edu": "Среднее образование", "courses": "-",
-                                                "skills": "Изготовление мебели", "add": "Добросовестный"})
+    return render(request, "resume/Base.html", {"fill": fill, "name": "Иван Васильевич", "birthday": "12.02.84"})
